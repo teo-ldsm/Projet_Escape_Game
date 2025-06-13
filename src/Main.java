cdd4a0a12454bdd75a3c767b939756b89c85411f
Clément <clementcano95@gmail.com>
2025-06-13 15:21:27 +0200

    ajout une partie des scen

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,29 @@ public class Main {
     public static void main(String[] args) {
         Fenetre fenetre = new Fenetre();
 
-        Scene bureauSombre = new Scene("src/resources/Salle sombre.png", "Un homme dans la rue vous " +
-                "a proposé de monter dans sa camionnette blanche pour vous donner des bonbons. Il vous a kidnappé et " +
-                "drogué, vous vous trouvez dans une cave sombre. Vous devez trouver un moyen d'allumer la lumière " +
-                "pour vous échapper.");
+        Scene bureauSombre = new Scene("src/resources/Salle sombre.png", "Un homme dans la rue vous " +"a proposé de monter dans sa camionnette blanche pour vous donner des bonbons. Il vous a kidnappé et " +"drogué, vous vous trouvez dans une cave sombre. Vous devez trouver un moyen d'allumer la lumière " +"pour vous échapper.");
+
         Scene salleFlippante = new Scene("src/resources/Salle flippante.png", "Vous êtes dans une salle flippante, " + "vous devez trouver un moyen de sortir.");
+
+        Scene sactelephone = new Scene("src/resources/Salle téléphone.png", "C'est un sac contenant un téléphone, des médicaments, un tournevis et des serviettes, ");
+
+        Scene photocarnet = new Scene("src/resources/Photocarnet.png", "C'est un carnet contenant des notes, ");
+
+        
 
         fenetre.scenes.add(bureauSombre);
         
         Objet interrupteur = new Objet(212, 250, 90, 220, "Interrupteur", "Un interrupteur qui pourrait allumer la lumière.", true, Hitboxes.CARRE);
         bureauSombre.objets.add(interrupteur);
         bureauSombre.afficher();
-        
         while (fenetre.currentScene == bureauSombre) {
             if (interrupteur.used==true){
                 fenetre.scenes.add(salleFlippante);
                 salleFlippante.afficher();
                 }
-            
+            fenetre.repaint();
         }
+        // allumer la lumière
 
-//        Objet interrupteur = new Objet();
     }
 }
