8682d9cbf30c97fd96ef4854b39e0a2ad05a86f1
Clément <clementcano95@gmail.com>
2025-06-13 09:14:36 +0200

    txt

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Fenetre fenetre = new Fenetre();
-
         fenetre.afficheimage("src/resources/Salle sombre.png");
-        fenetre.bandeau.setText("Bandeau");
-
+        fenetre.bandeau.setText("Un homme dans la rue vous a proposé de monter dans sa camionnette blanche pour vous donner des bonbons. Il vous a kidnappé et drogué, vous vous trouvez dans une cave sombre. Vous devez trouver un moyen d'allumer la lumière pour vous échapper.");
+        new Fenetre();
     }
 }
